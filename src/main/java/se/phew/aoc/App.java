f167f29c0328b8600ed5d113ac47a4dca2bec538
Martin Åberg <martin.aberg@kindredgroup.com>
2020-12-27 14:14:48 +0100

    Rewrote Day 1 to print the answer to both part 1 and part 2

--- src/main/java/se/phew/aoc/App.java
+++ src/main/java/se/phew/aoc/App.java
@@ -7,13 +7,13 @@ public class App {
     public static void main(String[] args) {
         // PropertyConfigurator.configure("log4j.properties");
 
-        /*new Day1();
-        new Day2();
+        new Day1();
+        /*new Day2();
         new Day3();
         new Day4();
         new Day5();*/
         //new Day18();
-        new Day19();
+        // new Day19();
         // new Day21();
     }
 }
--- src/main/java/se/phew/aoc/days/Day1.java
+++ src/main/java/se/phew/aoc/days/Day1.java
@@ -12,13 +12,21 @@ public class Day1 extends Challenge {
             set.add(Integer.parseInt(line));
         }
 
+        for (int i : set) {
+            int pair = 2020 - i;
+            if (set.contains(pair)) {
+                System.out.println("Part 1 answer: " + i * pair + " (" + i + " * " + pair + ")");
+                break;
+            }
+        }
+
         loop:
         for (int i : set) {
             int remaining = 2020 - i;
             for (int j : set) {
                 int temp = remaining - j;
                 if (set.contains(temp)) {
-                    System.out.println("Found a pair; " + i + " * " + j + " * " + temp + " = " + i * j * temp);
+                    System.out.println("Part 2 answer: " + i * j * temp + " (" + i + " * " + j + " * " + temp + ")");
                     break loop;
                 }
             }
