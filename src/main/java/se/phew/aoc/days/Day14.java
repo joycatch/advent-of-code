f0c49613791367293fb21197221aec5755289b75
Martin Åberg <martin.aberg@kindredgroup.com>
2020-12-28 02:04:28 +0100

    Solved Day 14 part 2

--- src/main/java/se/phew/aoc/days/Day14.java
+++ src/main/java/se/phew/aoc/days/Day14.java
@@ -1,5 +1,6 @@
 package se.phew.aoc.days;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Day14 extends Challenge {
@@ -7,31 +8,68 @@ public class Day14 extends Challenge {
     public Day14() {
         super();
 
-        HashMap<Integer, Long> memory = new HashMap<>();
+        HashMap<Long, Long> memoryPart1 = new HashMap<>();
+        HashMap<Long, Long> memoryPart2 = new HashMap<>();
 
         String currentMask = "";
-        for (String line : lines) {
+        for (String originalLine : lines) {
+            String line = originalLine;
             if (line.contains("mask = ")) {
                 currentMask = line.substring("mask = ".length());
-                System.out.println(currentMask);
             } else {
                 line = line.replaceAll("mem\\[", "");
                 String[] split = line.split("] = ");
-                int position = Integer.parseInt(split[0]);
-                int value = Integer.parseInt(split[1]);
-                String binary = String.format("%36s", Integer.toBinaryString(value)).replace(' ', '0');
-                System.out.println(binary + " <- Original");
-                String newBinary = applyMask(currentMask, binary);
-                System.out.println(newBinary + " <- New");
-                memory.put(position, getValue(newBinary));
+                long position = Long.parseLong(split[0]);
+                long value = Long.parseLong(split[1]);
+                String binaryValue = String.format("%36s", Integer.toBinaryString((int) value)).replace(' ', '0');
+                String newBinaryValue = applyMaskPart1(currentMask, binaryValue);
+                memoryPart1.put(position, getValue(newBinaryValue));
+
+                String binaryPosition = String.format("%36s", Integer.toBinaryString((int) position)).replace(' ', '0');
+                ArrayList<String> newBinaryPositions = applyMaskPart2(currentMask, binaryPosition);
+                for (String newPosition : newBinaryPositions) {
+                    memoryPart2.put(getValue(newPosition), value);
+                }
             }
         }
 
         long result = 0;
-        for (Long value : memory.values()) {
+        for (Long value : memoryPart1.values()) {
             result += value;
         }
         printAnswer(1, result);
+
+        result = 0;
+        for (Long value : memoryPart2.values()) {
+            result += value;
+        }
+        printAnswer(2, result);
+    }
+
+    private ArrayList<String> applyMaskPart2(String currentMask, String binary) {
+        String mask = "";
+        for (int i = 0; i < currentMask.length(); i++) {
+            switch(currentMask.charAt(i)) {
+                case '0': mask += binary.charAt(i); break;
+                case '1': mask += '1'; break;
+                case 'X': mask += 'X'; break;
+            }
+        }
+        // System.out.println(mask);
+        ArrayList<String> result = new ArrayList<>();
+
+        generateMasks(mask, result);
+
+        return result;
+    }
+
+    private void generateMasks(String binary, ArrayList<String> result) {
+        if (!binary.contains("X")) {
+            result.add(binary);
+        } else {
+            generateMasks(binary.replaceFirst("X", "0"), result);
+            generateMasks(binary.replaceFirst("X", "1"), result);
+        }
     }
 
     private long getValue(String binary) {
@@ -44,7 +82,7 @@ public class Day14 extends Challenge {
         return result;
     }
 
-    private String applyMask(String currentMask, String binary) {
+    private String applyMaskPart1(String currentMask, String binary) {
         String result = "";
         for (int i = 0; i < currentMask.length(); i++) {
             switch(currentMask.charAt(i)) {
