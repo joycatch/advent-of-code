deb0498cf3628722317b9fa7ba46cca226c49303
martin@phew.se <martin@phew.se>
2021-01-03 15:06:09 +0100

    Add Day 6

--- src/main/java/se/phew/aoc/App.java
+++ src/main/java/se/phew/aoc/App.java
@@ -7,16 +7,29 @@ public class App {
     public static void main(String[] args) {
         // PropertyConfigurator.configure("log4j.properties");
 
-        //new Day1();
-        //new Day2();
-        /*new Day3();
-        new Day4();
-        new Day5();*/
+        // new Day1();
+        // new Day2();
+        // new Day3();
+        // new Day4();
+        // new Day5();
+        new Day6();
+        // new Day7();
+        // new Day8();
+        // new Day9();
+        // new Day10();
+        // new Day11();
+        // new Day12();
+        // new Day13();
         // new Day14();
-        new Day10();
-        //new Day17();
+        // new Day15();
+        // new Day16();
+        // new Day17();
         // new Day18();
         // new Day19();
-        //new Day21();
+        // new Day20();
+        // new Day21();
+        // new Day22();
+        // new Day23();
+        // new Day24();
     }
 }
---
+++ src/main/java/se/phew/aoc/days/Day6.java
@@ -0,0 +1,64 @@
+package se.phew.aoc.days;
+
+import java.util.HashMap;
+
+public class Day6 extends Challenge {
+
+    public Day6() {
+        super();
+
+        GroupVotes groupVotes = new GroupVotes();
+        int part1 = 0;
+        int part2 = 0;
+
+        for (String line : lines) {
+            if (!line.isEmpty()) {
+                groupVotes.contributors++;
+                for (char ch : line.toCharArray()) {
+                    groupVotes.add(ch);
+                }
+            } else {
+                part1 += groupVotes.getAnybodyAnsweredYesCount();
+                part2 += groupVotes.getEverybodyAnsweredYesCount();
+                groupVotes = new GroupVotes();
+            }
+        }
+        part1 += groupVotes.getAnybodyAnsweredYesCount();
+        part2 += groupVotes.getEverybodyAnsweredYesCount();
+
+        printAnswer(1, part1);
+        printAnswer(2, part2);
+    }
+
+
+    static class GroupVotes {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int contributors = 0;
+
+        void add(char key) {
+            if (map.get(key) == null) {
+                map.put(key, 1);
+            } else {
+                map.put(key, map.get(key) + 1);
+            }
+        }
+
+        int getAnybodyAnsweredYesCount() {
+            return map.keySet().size();
+        }
+
+        int getEverybodyAnsweredYesCount() {
+            int result = 0;
+            for (Character c : map.keySet()) {
+                if (map.get(c) == contributors) {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+        public String toString() {
+            return map.toString();
+        }
+    }
+}
