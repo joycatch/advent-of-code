f41d3f9073bbcb02d4f622e8410ccfe1b803c278
martin@phew.se <martin@phew.se>
2021-01-08 00:58:31 +0100

    Solved Day 22 - part 1

--- src/main/java/se/phew/aoc/App.java
+++ src/main/java/se/phew/aoc/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         // PropertyConfigurator.configure("log4j.properties");
 
-        new Day1();
+        /*new Day1();
         new Day2();
         // new Day3();
         // new Day4();
@@ -21,14 +21,14 @@ public class App {
         // new Day12();
         new Day13();
         // new Day14();
-        new Day15();
+        new Day15();*/
         // new Day16();
         // new Day17();
         // new Day18();
         // new Day19();
         // new Day20();
         // new Day21();
-        // new Day22();
+        new Day22();
         // new Day23();
         // new Day24();
         // new Day25();
--- src/main/java/se/phew/aoc/days/Day22.java
+++ src/main/java/se/phew/aoc/days/Day22.java
@@ -1,15 +1,92 @@
 package se.phew.aoc.days;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Day22 extends Challenge {
-
-    HashMap<String, String> map = new HashMap<>();
 
     public Day22() {
         super();
 
-        // TBD
+        // Part 1
+        Player p1 = new Player("Player 1");
+        Player p2 = new Player("Player 2");
 
+        Player player = p1;
+        for (String line : lines) {
+            if (line.startsWith(p1.name)) {
+                player = p1;
+            } else if (line.startsWith(p2.name)) {
+                player = p2;
+            } else {
+                if (!line.isEmpty()) {
+                    player.addCardToDeck(Integer.parseInt(line));
+                }
+            }
+        }
+
+        do {
+            int p1card = p1.playCard();
+            int p2card = p2.playCard();
+
+            if (p1card > p2card) {
+                p1.addToBottom(p1card, p2card);
+            } else {
+                p2.addToBottom(p2card, p1card);
+            }
+        } while ( p1.deckIsNotEmpty() && p2.deckIsNotEmpty());
+
+        p1.printDeck();
+        p2.printDeck();
+
+        int p1score = p1.calculateScore();
+        int p2score = p2.calculateScore();
+        int result = Math.max(p1score, p2score);
+        printAnswer(1, result);
+
+        // Part 2
+
+        // TBD
+    }
+
+    private class Player {
+        String name;
+        Queue<Integer> deck = new LinkedList<>();
+
+        public Player(String name) {
+            this.name = name;
+        }
+
+        public void addCardToDeck(int card) {
+            deck.add(card);
+        }
+
+        public int playCard() {
+            return deck.poll();
+        }
+
+        public void addToBottom(int winningCard, int losingCard) {
+            deck.add(winningCard);
+            deck.add(losingCard);
+        }
+
+        public boolean deckIsNotEmpty() {
+            return deck.size() > 0;
+        }
+
+        public void printDeck() {
+            String result = deck.stream().map(a -> String.valueOf(a)).collect(Collectors.joining(","));
+            print(name + "'s deck: " + result);
+        }
+
+        public int calculateScore() {
+            int size = deck.size();
+            int result = 0;
+            for (int i = size; i > 0; i--) {
+                result += i*deck.poll();
+            }
+            return result;
+        }
     }
 }
