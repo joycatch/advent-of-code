f32b4a2ff7d4ee15c9e7ec0f2458bddd774ed45e
martin@phew.se <martin@phew.se>
2021-01-12 19:33:45 +0100

    Solved Day 23 - part 1

--- src/main/java/se/phew/aoc/App.java
+++ src/main/java/se/phew/aoc/App.java
@@ -26,10 +26,10 @@ public class App {
         // new Day17();
         // new Day18();
         // new Day19();
-        new Day20();
+        // new Day20();
         // new Day21();
         // new Day22();
-        // new Day23();
+        new Day23();
         // new Day24();
         // new Day25();
     }
--- src/main/java/se/phew/aoc/days/Day23.java
+++ src/main/java/se/phew/aoc/days/Day23.java
@@ -1,15 +1,75 @@
 package se.phew.aoc.days;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Day23 extends Challenge {
-
-    HashMap<String, String> map = new HashMap<>();
 
     public Day23() {
         super();
 
-        // TBD
+        String input = "156794823";
+        if (isTest) {
+            input = "389125467";
+        }
 
+        Queue<Integer> list = new LinkedList<>();
+        for (char number : input.toCharArray()) {
+            list.add(Integer.parseInt("" + number));
+        }
+
+        int[] pickedUp = new int[3];
+
+        for (int move = 1; move <= 100; move++) {
+            System.out.println("\n-- move " + move + " --");
+            System.out.print("cups: ");
+            for (int i = 0; i < input.length(); i++) {
+                int n = list.poll();
+                System.out.print((i == 0 ? "(" + n + ")" : n ) + " ");
+                list.add(n);
+            }
+            System.out.print("\npick up: ");
+            int number = list.poll();
+            for (int j = 0; j < 3; j++) {
+                pickedUp[j] = list.poll();
+                System.out.print(pickedUp[j] + ", ");
+            }
+            list.add(number);
+            list = findDestination(list, number);
+            for (int j = 0; j < 3; j++) {
+                list.add(pickedUp[j]);
+            }
+            rotateTo(list, number);
+        }
+
+        rotateTo(list, 1);
+        list.remove(1);
+        String answer = "";
+        for (int i : list) {
+            answer += "" + i;
+        }
+
+        print("");
+        printAnswer(1, answer);
+    }
+
+    private Queue<Integer> rotateTo(Queue<Integer> list, int number) {
+        int lastNumber;
+        do {
+            lastNumber = list.poll();
+            list.add(lastNumber);
+        } while (lastNumber != number);
+        return list;
+    }
+
+    private Queue<Integer> findDestination(Queue<Integer> list, int needle) {
+        int min = list.stream().reduce(Integer::min).get();
+        do {
+            if (--needle < min) {
+                needle = list.stream().reduce(Integer::max).get();
+            }
+        } while (!list.contains(needle));
+        System.out.println("\ndestination: " + needle);
+        return rotateTo(list, needle);
     }
 }
