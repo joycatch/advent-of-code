09b4100415f6d74176e04ff56d6006d78372c5f0
martin@phew.se <martin@phew.se>
2021-01-20 00:32:52 +0100

    Solved Day 1 for 2019

--- src/main/java/se/phew/aoc/days/twenty19/Day01.java
+++ src/main/java/se/phew/aoc/days/twenty19/Day01.java
@@ -7,11 +7,25 @@ public class Day01 extends Challenge {
     public Day01() {
         super();
 
+        long part1 = 0;
+        long part2 = 0;
         for (String line : lines) {
-            // TBD
+            int mass = Integer.parseInt("" + line);
+            int fuel = (int) (mass/3 - 2);
+            part1 += fuel;
+            part2 += fuel + fuelFor(fuel);
         }
 
-        printAnswer(1, "TBD");
-        printAnswer(2, "TBD");
+        printAnswer(1, part1);
+        printAnswer(2, part2);
+    }
+
+    private long fuelFor(int mass) {
+        int result = (int) (mass/3 - 2);
+        if (result < 0) {
+            return 0;
+        } else {
+            return result + fuelFor(result);
+        }
     }
 }
