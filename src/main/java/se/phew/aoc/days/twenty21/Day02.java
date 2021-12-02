76f3909aceb8d47f119f43308a29f9237bd52a33
Martin Åberg <martin.aberg@klarna.com>
2021-12-02 12:24:13 +0100

    Updated Day 2 for 2021

--- src/main/java/se/phew/aoc/days/twenty21/Day02.java
+++ src/main/java/se/phew/aoc/days/twenty21/Day02.java
@@ -7,19 +7,19 @@ public class Day02 extends Challenge {
     public Day02() {
         super();
 
-        Submarine submarine = new Submarine();
-        followInstructions(submarine, true);
+        Submarine submarine = new Submarine(false);
+        followInstructions(submarine);
         printAnswer(1, submarine.position());
 
-        submarine = new Submarine();
-        followInstructions(submarine, false);
+        submarine = new Submarine(true);
+        followInstructions(submarine);
         printAnswer(2, submarine.position());
     }
 
-    private void followInstructions(Submarine submarine, boolean part1) {
+    private void followInstructions(Submarine submarine) {
         for (String line : lines) {
             String[] split = line.split(" ");
-            submarine.move(split[0], Integer.parseInt(split[1]), part1);
+            submarine.move(split[0], Integer.parseInt(split[1]));
         }
     }
 
@@ -27,24 +27,29 @@ public class Day02 extends Challenge {
         int x = 0;
         int y = 0;
         int aim = 0;
+        boolean useAim;
 
-        public void move(String instruction, int steps, boolean part1) {
+        public Submarine(boolean useAim) {
+            this.useAim = useAim;
+        }
+
+        public void move(String instruction, int steps) {
             switch (instruction) {
                 case "forward":
                     x += steps;
-                    if (!part1) {
+                    if (useAim) {
                         y += aim * steps;
                     }
                     break;
                 case "down":
-                    if (part1) {
+                    if (!useAim) {
                         y += steps;
                     } else {
                         aim += steps;
                     }
                     break;
                 case "up":
-                    if (part1) {
+                    if (!useAim) {
                         y -= steps;
                     } else {
                         aim -= steps;
