cf1e031d32b3c80a912db4eef4fc37eac4ac6bbc
Martin Åberg <martin.aberg@klarna.com>
2021-12-03 16:44:37 +0100

    Solved Day 3 for 2021

--- src/main/java/se/phew/aoc/days/twenty21/Day03.java
+++ src/main/java/se/phew/aoc/days/twenty21/Day03.java
@@ -2,12 +2,55 @@ package se.phew.aoc.days.twenty21;
 
 import se.phew.aoc.days.Challenge;
 
+import java.util.ArrayList;
+
 public class Day03 extends Challenge {
 
     public Day03() {
         super();
 
-        printAnswer(1, "");
-        printAnswer(2, "");
+        String gamma = "";
+        String epsilon = "";
+
+        int[] count = new int[lines.get(0).length()];
+        for (String line : lines) {
+            for (int i = 0; i < count.length; i++) {
+                count[i] += Character.getNumericValue(line.charAt(i));
+            }
+        }
+        for (int i = 0; i < count.length; i++) {
+            gamma += String.valueOf(count[i] >= (double) lines.size() / 2 ? '1' : '0');
+            epsilon += String.valueOf(count[i] >= (double) lines.size() / 2 ? '0' : '1');
+        }
+
+        ArrayList<String> oxygen = new ArrayList<>(lines);
+        ArrayList<String> CO2 = new ArrayList<>(lines);
+
+        filter(oxygen, '1', '0');
+        filter(CO2, '0', '1');
+
+        printAnswer(1, Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2));
+        printAnswer(2, Integer.parseInt(oxygen.get(0), 2) * Integer.parseInt(CO2.get(0), 2));
+    }
+
+    private void filter(ArrayList<String> list, char most, char least) {
+        for (int i = 0; list.size() > 1; i++) {
+            filter(list, i, most, least);
+        }
+    }
+
+    private void filter(ArrayList<String> list, int i, char most, char least) {
+        int count = 0;
+        for (String line : list) {
+            count += Character.getNumericValue(line.charAt(i));
+        }
+        char toTestAgainst = count >= (double) list.size() / 2 ? most : least;
+        ArrayList<String> discard = new ArrayList<>();
+        for (String line : list) {
+            if (line.charAt(i) != toTestAgainst) {
+                discard.add(line);
+            }
+        }
+        list.removeAll(discard);
     }
 }
