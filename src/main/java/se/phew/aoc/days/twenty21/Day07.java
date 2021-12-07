c9aefe66c6d064fb5ed89ebe65972c6b65d3d475
Martin Åberg <martin.aberg@klarna.com>
2021-12-07 09:42:30 +0100

    Solved Day 7 for 2021

--- src/main/java/se/phew/aoc/days/twenty21/Day07.java
+++ src/main/java/se/phew/aoc/days/twenty21/Day07.java
@@ -2,12 +2,46 @@ package se.phew.aoc.days.twenty21;
 
 import se.phew.aoc.days.Challenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Day07 extends Challenge {
 
     public Day07() {
         super();
 
-        printAnswer(1, "");
-        printAnswer(2, "");
+        ArrayList<Integer> positions = new ArrayList<>();
+        for (String split : lines.get(0).split(",")) {
+            positions.add(Integer.parseInt(split));
+        }
+
+        Collections.sort(positions);
+
+        printAnswer(1, countFuel(positions, true));
+        printAnswer(2, countFuel(positions, false));
+    }
+
+    private int countFuel(ArrayList<Integer> positions, boolean part1) {
+        int min;
+        min = Integer.MAX_VALUE;
+        for (int i = positions.get(0); i <= positions.get(positions.size() - 1); i++) {
+            int result = 0;
+            for (Integer submarine : positions) {
+                int cost;
+                if (part1) {
+                    cost = Math.abs(submarine - i);
+                } else {
+                    cost = 0;
+                    for (int j = 1; j <= Math.abs(submarine - i); j++) {
+                        cost += j;
+                    }
+                }
+                result += cost;
+            }
+            if (result < min) {
+                min = result;
+            }
+        }
+        return min;
     }
 }
