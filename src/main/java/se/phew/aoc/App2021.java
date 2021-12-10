1fc6bc881f54903a289c10817f1bbcfbe4afed3a
Martin Åberg <martin.aberg@klarna.com>
2021-12-10 16:38:46 +0100

    Solved Day 10 for 2021

--- src/main/java/se/phew/aoc/App2021.java
+++ src/main/java/se/phew/aoc/App2021.java
@@ -14,7 +14,7 @@ public class App2021 {
         new Day07();
         new Day08();
         new Day09();
-        // new Day10();
+        new Day10();
         // new Day11();
         // new Day12();
         // new Day13();
--- src/main/java/se/phew/aoc/days/twenty21/Day10.java
+++ src/main/java/se/phew/aoc/days/twenty21/Day10.java
@@ -2,12 +2,72 @@ package se.phew.aoc.days.twenty21;
 
 import se.phew.aoc.days.Challenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Day10 extends Challenge {
 
     public Day10() {
         super();
 
-        printAnswer(1, "");
-        printAnswer(2, "");
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : "([{<>}])".toCharArray()) {
+            map.put(c, 0);
+        }
+        ArrayList<Long> scores = new ArrayList<>();
+
+        for (String line : lines) {
+            Stack<Character> stack = new Stack<>();
+            boolean isCorrupt = false;
+            for (char c : line.toCharArray()) {
+                if (isOpener(c)) {
+                    stack.add(c);
+                } else {
+                    if (getCloser(stack.pop()) != c) {
+                        map.put(c, map.get(c) + 1);
+                        isCorrupt = true;
+                        break;
+                    }
+                }
+            }
+            if (!isCorrupt) {
+                long count = 0;
+                while (!stack.isEmpty()) {
+                    count = 5 * count + getCloserPoints(getCloser(stack.pop()));
+                }
+                scores.add(count);
+            }
+        }
+
+        Collections.sort(scores);
+
+        printAnswer(1, map.get(')') * 3 + map.get(']') * 57 + map.get('}') * 1197 + map.get('>') * 25137);
+        printAnswer(2, scores.get((scores.size()/2)));
+    }
+
+    private int getCloserPoints(char c) {
+        switch (c) {
+            case ')': return 1;
+            case ']': return 2;
+            case '}': return 3;
+            case '>': return 4;
+        }
+        return -1;
+    }
+
+    private char getCloser(char c) {
+        switch (c) {
+            case '(': return ')';
+            case '[': return ']';
+            case '{': return '}';
+            case '<': return '>';
+        }
+        return '?';
+    }
+
+    private boolean isOpener(char c) {
+        return c == '(' || c == '[' || c == '{' || c == '<' ;
     }
 }
