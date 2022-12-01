d5d32b8f8c1be922b477d4370db2fd092b0a7ad2
Martin Åberg <martin.aberg@arbetsformedlingen.se>
2022-12-01 14:58:20 +0100

    Refactor Day 1 for 2022

--- src/main/java/se/phew/aoc/days/twenty22/Day01.java
+++ src/main/java/se/phew/aoc/days/twenty22/Day01.java
@@ -8,30 +8,25 @@ import java.util.List;
 
 public class Day01 extends Challenge {
 
-    ArrayList<Integer> sums = new ArrayList<>();
-
     public Day01() {
         super();
+
+        ArrayList<Integer> sums = new ArrayList<>();
 
         List<Integer> calories = new ArrayList<>();
         for (String line : lines) {
             if (!line.isEmpty()) {
                 calories.add(Integer.parseInt(line));
             } else {
-                addSum(calories);
+                sums.add(calories.stream().mapToInt(Integer::intValue).sum());
                 calories = new ArrayList<>();
             }
         }
-        addSum(calories);
+        sums.add(calories.stream().mapToInt(Integer::intValue).sum());
 
         Collections.sort(sums);
 
         printAnswer(1, sums.get(sums.size() - 1));
         printAnswer(2, sums.get(sums.size() - 1) + sums.get(sums.size() - 2) + sums.get(sums.size() - 3));
-    }
-
-    void addSum(List<Integer> calories) {
-        int sum = calories.stream().mapToInt(Integer::intValue).sum();
-        sums.add(sum);
     }
 }
