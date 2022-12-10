c61fb7ab8c1397c883eb259d52f9f32c19781f7a
martin@phew.se <martin@phew.se>
2022-12-10 02:07:33 +0100

    Solved Day 9 for 2022

--- src/main/java/se/phew/aoc/days/twenty22/Day09.java
+++ src/main/java/se/phew/aoc/days/twenty22/Day09.java
@@ -9,74 +9,35 @@ public class Day09 extends Challenge {
     public Day09() {
         super(false);
 
-        Rope ropePart1 = new Rope(1);
-        Rope ropePart2 = new Rope(10);
+        Rope rope1 = new Rope(2);
+        Rope rope2 = new Rope(10);
 
         for (String line : lines) {
             String[] s = line.split(" ");
-
             for (int i = 0; i < Integer.parseInt(s[1]); i++) {
-                ropePart1.move(s[0]);
-                ropePart2.move(s[0]);
-                //printTest(ropePart2);
+                rope1.move(s[0]);
+                rope2.move(s[0]);
             }
         }
 
-        printAnswer(1, ropePart1.tail().countLocations());
-        printAnswer(2, ropePart2.tail().countLocations());
-    }
-
-    private void printTest(Rope ropePart2) {
-        int max = 5;
-        for (int y = max; y > -max; y--) {
-            for (int x = -max; x < max; x++) {
-                boolean found = false;
-                if (ropePart2.headX == x && ropePart2.headY == y) {
-                    System.out.print("H");
-                    found = true;
-                } else {
-                    for (Part part : ropePart2.parts) {
-                        if (part.x == x && part.y == y) {
-                            System.out.print(part.name);
-                            found = true;
-                            break;
-                        }
-                    }
-                }
-                if (!found) {
-                    System.out.print(".");
-                }
-            }
-            System.out.println("");
-        }
-
-        System.out.println();
-        System.out.println();
+        printAnswer(1, rope1.tail().countLocations());
+        printAnswer(2, rope2.tail().countLocations());
     }
 
     class Rope {
-        int headX, headY;
         List<Part> parts = new ArrayList<>();
 
         public Rope(int length) {
-            for (int i = 1; i < length + 1; i++) {
-                parts.add(new Part(String.valueOf(i)));
+            for (int i = 0; i < length; i++) {
+                parts.add(new Part(i == 0 ? "H" : String.valueOf(i)));
             }
         }
 
         public void move(String direction) {
-            switch (direction) {
-                case "R": headX++; break;
-                case "U": headY++; break;
-                case "D": headY--; break;
-                case "L": headX--; break;
-            }
-            int x = headX;
-            int y = headY;
+            Part previous = null;
             for (Part part : parts) {
-                part.move(direction, x, y);
-                x = part.x;
-                y = part.y;
+                part.move(direction, previous);
+                previous = part;
             }
         }
 
@@ -100,44 +61,27 @@ public class Day09 extends Challenge {
             return locations.size();
         }
 
-        public void move(String direction, int previousX, int previousY) {
-            int diffX = Math.abs(previousX - x);
-            int diffY = Math.abs(previousY - y);
-            int diff = diffX + diffY;
-            switch (diff) {
-                case 1:
-                    break;
-                case 2:
-                    if (diffX == 0 || diffY == 0) {
-                        switch (direction) {
-                            case "R": x++; break;
-                            case "U": y++; break;
-                            case "D": y--; break;
-                            case "L": x--; break;
-                        }
-                    }
-                    break;
-                case 3:
-                    x = previousX;
-                    y = previousY;
-                    switch (direction) {
-                        case "R": x--; break;
-                        case "U": y--; break;
-                        case "D": y++; break;
-                        case "L": x++; break;
-                    }
-                    break;
-                case 4:
-                    switch (direction) {
-                        case "R": x++; break;
-                        case "U": y++; break;
-                        case "D": y--; break;
-                        case "L": x--; break;
-                    }
-                    break;
+        public void move(String direction, Part previous) {
+            if (previous == null) {
+                switch (direction) {
+                    case "R": x++; break;
+                    case "U": y++; break;
+                    case "D": y--; break;
+                    case "L": x--; break;
+                }
+                return;
             }
+            int diffX = (previous.x - x);
+            int diffY = (previous.y - y);
+
+            if (Math.abs(diffX) <= 1 && Math.abs(diffY) <= 1) {
+                return;
+            }
+
+            y += Integer.signum(diffY);
+            x += Integer.signum(diffX);
+
             String location = x + "," + y;
-            // System.out.println(location);
             locations.add(location);
         }
     }
