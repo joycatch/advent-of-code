eabea87103449121e55ca7c05b6120514471227e
martin@phew.se <martin@phew.se>
2022-12-12 23:04:24 +0100

    Solved Day 12 for 2022

--- src/main/java/se/phew/aoc/days/twenty22/Day12.java
+++ src/main/java/se/phew/aoc/days/twenty22/Day12.java
@@ -2,20 +2,24 @@ package se.phew.aoc.days.twenty22;
 
 import se.phew.aoc.days.Challenge;
 
+import java.util.*;
+
 public class Day12 extends Challenge {
 
     int[][] offsets = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
     int[][] map;
-    int yMax, xMax, startY, startX, goalY, goalX, currentX, currentY;
+    int rows, columns, startY, startX, goalY, goalX;
+    HashMap<String, Node> nodes;
 
     public Day12() {
-        super(true);
+        super(false);
 
-        yMax = lines.size();
-        xMax = lines.get(0).length();
-        map = new int[yMax][xMax];
+        rows = lines.size();
+        columns = lines.get(0).length();
+        map = new int[rows][columns];
+        nodes = new HashMap<>();
 
-        for (int y = 0; y < yMax; y++) {
+        for (int y = 0; y < rows; y++) {
             int x = 0;
             for (char c : lines.get(y).toCharArray()) {
                 int value = (int) c - (int) 'a';
@@ -29,20 +33,85 @@ public class Day12 extends Challenge {
                     goalX = x;
                     value = 1 + (int) 'z' - (int) 'a';
                 }
-                map[y][x++] = value;
+                nodes.put(getNodeName(y, x), new Node(y, x, value));
+                x++;
             }
         }
 
-        int steps = 0;
-        currentY = startY;
-        currentX = startX;
-        // while (!atGoal()) {
+        for (Node node : nodes.values()) {
+            setNeighbours(node);
+        }
+        Node start = nodes.get(getNodeName(startY, startX));
+        Node goal = nodes.get(getNodeName(goalY, goalX));
 
-        //}
+        printAnswer(1, shortestDistanceFrom(start, goal));
 
+        int shortest = Integer.MAX_VALUE;
+        for (Node node : nodes.values()) {
+            if (node.value == 0) {
+                int distance = shortestDistanceFrom(node, goal);
+                if (distance > 0 && distance < shortest) {
+                    shortest = distance;
+                }
+            }
+        }
+
+        printAnswer(2, shortest);
     }
 
-    private boolean atGoal() {
-        return currentX == goalX && currentY == goalY;
+    private int shortestDistanceFrom(Node start, Node goal) {
+        nodes.values().stream().forEach(n -> n.distance = 0);
+
+        Set<Node> visited = new HashSet<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current == goal) {
+                return current.distance;
+            }
+
+            for (Node node : current.neighbours) {
+                node.distance = current.distance + 1;
+                if (!visited.contains(node)) {
+                    visited.add(node);
+                    queue.add(node);
+                }
+            }
+        }
+        return -1;
+    }
+
+    private String getNodeName(int y, int x) {
+        return y + "," + x;
+    }
+
+    private void setNeighbours(Node node) {
+        for (int[] offset : offsets) {
+            int ix = node.x + offset[0];
+            int iy = node.y + offset[1];
+            if (ix < 0 || ix >= columns || iy < 0 || iy >= rows) {
+                continue;
+            }
+            Node other = nodes.get(getNodeName(iy, ix));
+            if (other.value <= node.value + 1) {
+                node.neighbours.add(other);
+            }
+        }
+    }
+
+    class Node {
+        int y, x, value;
+        private Set<Node> neighbours = new HashSet<>();
+        private Integer distance = 0;
+
+        public Node(int y, int x, int value) {
+            this.y = y;
+            this.x = x;
+            this.value = value;
+        }
     }
 }
