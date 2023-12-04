8bd7f3269d71b3ccb288ac9b1cbe025351ca594f
Martin Åberg <martin.aberg@arbetsformedlingen.se>
2023-12-04 09:59:14 +0100

    Solve Day 3 for 2023

--- src/main/java/se/phew/aoc/days/twenty23/Day03.java
+++ src/main/java/se/phew/aoc/days/twenty23/Day03.java
@@ -6,14 +6,15 @@ import java.util.*;
 
 public class Day03 extends Challenge {
 
+    public static final char EMPTY_CHAR = '.';
     private static int[][] offsets = new int[][] { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1} };
     char[][] map;
     int rows, columns;
     List<Integer> list = new ArrayList<>();
-    HashMap<Character, List<Cordinates>> symbols = new HashMap<>();
+    HashMap<Symbol, List<Integer>> symbolToNumberMap = new HashMap<>();
 
     public Day03() {
-        super(true);
+        super(false);
 
         rows = lines.size();
         columns = lines.get(0).length();
@@ -24,29 +25,6 @@ public class Day03 extends Challenge {
         }
 
         for (int y = 0; y < rows; y++) {
-            for (int x = 0; x < columns; x++) {
-                if (isSymbol(y, x)) {
-                    symbols.putIfAbsent(map[y][x], new ArrayList<>());
-                    symbols.get(map[y][x]).add(new Cordinates(y, x));
-                }
-            }
-        }
-
-        int sum = 0;
-        for (Character symbol : symbols.keySet()) {
-            for (Cordinates cordinates : symbols.get(symbol)) {
-                Set<Integer> numbers = findAdjacentNumbers(cordinates.y, cordinates.x);
-                if (numbers.size() == 2) {
-                    System.out.println("Two numbers: " + numbers);
-                }
-                sum += numbers.stream().mapToInt(Integer::intValue).sum();
-            }
-        }
-        printAnswer(2, sum);
-
-
-
-        for (int y = 0; y < rows; y++) {
             int startX = -1, endX = -1;
             for (int x = 0; x < columns; x++) {
                 if (Character.isDigit(map[y][x])) {
@@ -54,99 +32,83 @@ public class Day03 extends Challenge {
                     while (x < columns && Character.isDigit(map[y][x])) {
                         endX = x++;
                     }
-                    addToListIfisAdjacentToASymbol(y, startX, endX);
+                    addNumberAndSymbol(y, startX, endX);
                 }
             }
         }
 
+        int part2 = 0;
+        for (Symbol symbol : symbolToNumberMap.keySet()) {
+            if (symbol.c == '*' && symbolToNumberMap.get(symbol).size() > 1) {
+                part2 += symbolToNumberMap.get(symbol).stream().reduce(1, (a, b) -> a * b);
+            }
+        }
+
         printAnswer(1, list.stream().mapToInt(Integer::intValue).sum());
-    }
-
-    private Set<Integer> findAdjacentNumbers(int y, int x) {
-        Set<Integer> set = new HashSet<>();
-        for (int[] offset : offsets) {
-            int ix = x + offset[0];
-            int iy = y + offset[1];
-            if (ix < 0 || ix >= rows || iy < 0 || iy >= columns) {
-                continue;
-            }
-            int fullNumber = getFullNumber(iy, ix);
-            if (fullNumber > 0) {
-                set.add(fullNumber);
-            }
-        }
-        return set;
-    }
-
-    private int getFullNumber(int y, int x) {
-        int startX = x, endX = x;
-        while (Character.isDigit(map[y][startX]) && startX > 0) {
-            startX--;
-        }
-        while (Character.isDigit(map[y][endX]) && endX < columns - 1) {
-            endX++;
-        }
-        int number = 0;
-        for (int ix = 0; ix <= endX - startX; ix++) {
-            number += Character.getNumericValue(map[y][startX + ix]) * (int) Math.pow(10, (endX - startX) - ix);
-        }
-        return number;
+        printAnswer(2, part2);
     }
 
     private boolean isSymbol(int y, int x) {
-        return map[y][x] != '.' && !Character.isDigit(map[y][x]);
+        return map[y][x] != EMPTY_CHAR && !Character.isDigit(map[y][x]);
     }
 
-    private void addToListIfisAdjacentToASymbol(int y, int startX, int endX) {
-        if (isAdjacentToASymbol(y, startX, endX)) {
+    private void addNumberAndSymbol(int y, int startX, int endX) {
+        Set<Symbol> adjacentSymbols = getAdjacentSymbols(y, startX, endX);
+        if (!adjacentSymbols.isEmpty()) {
             int number = 0;
             for (int ix = 0; ix <= endX - startX; ix++) {
                 number += Character.getNumericValue(map[y][startX + ix]) * (int) Math.pow(10, (endX - startX) - ix);
             }
             list.add(number);
-        }
-    }
-
-    private boolean isAdjacentToASymbol(int y, int startX, int endX) {
-        for (int x = startX; x <= endX; x++) {
-            if (isAdjacentToASymbol(y, x)) {
-                return true;
+            for (Symbol symbol : adjacentSymbols) {
+                symbolToNumberMap.putIfAbsent(symbol, new ArrayList<>());
+                symbolToNumberMap.get(symbol).add(number);
             }
         }
-        return false;
     }
 
-    private boolean isAdjacentToASymbol(int y, int x) {
+    private Set<Symbol> getAdjacentSymbols(int y, int startX, int endX) {
+        Set<Symbol> symbols = new HashSet<>();
+        for (int x = startX; x <= endX; x++) {
+            symbols.addAll(getAdjacentSymbols(y, x));
+        }
+        return symbols;
+    }
+
+    private Set<Symbol> getAdjacentSymbols(int y, int x) {
+        Set<Symbol> set = new HashSet<>();
         for (int[] offset : offsets) {
             int ix = x + offset[0];
             int iy = y + offset[1];
             if (ix < 0 || ix >= rows || iy < 0 || iy >= columns) {
                 continue;
             }
-            if (map[iy][ix] != '.' && !Character.isDigit(map[iy][ix])) {
-                return true;
+            if (map[iy][ix] != EMPTY_CHAR && !Character.isDigit(map[iy][ix])) {
+                set.add(new Symbol(map[iy][ix], iy, ix));
             }
         }
-        return false;
+        return set;
     }
 }
 
-class Cordinates {
+class Symbol {
+    Character c;
     int y, x;
 
-    public Cordinates(int y, int x) {
+    public Symbol(Character c, int y, int x) {
+        this.c = c;
         this.y = y;
         this.x = x;
     }
 
     @Override
     public boolean equals(Object o) {
-        Cordinates that = (Cordinates) o;
-        return y == that.y && x == that.x ;
+        Symbol symbol = (Symbol) o;
+        return y == symbol.y && x == symbol.x && Objects.equals(c, symbol.c);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(y, x);
+        return Objects.hash(c, y, x);
     }
 }
